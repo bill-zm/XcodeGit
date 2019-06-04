84ebfe303ef0be15dac9c77898c446ce181c4892
aa <2552639267@qq.com>
2019-06-04 09:38:42 +0800

    Initial Commit

---
+++ XcodeGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XcodeGit
+//
+//  Created by aa on 2019/6/4.
+//  Copyright © 2019 aa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
